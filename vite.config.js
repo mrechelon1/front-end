e0d71a0eb777852700ea8a4bb63495c24d7f5999
mrechelon1 <139581772+mrechelon1@users.noreply.github.com>
2025-06-02 10:12:35 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/front-end', // or '/your-app/' or the appropriate base URL
-  build: {
-    // ... other build options
-  },
 })
